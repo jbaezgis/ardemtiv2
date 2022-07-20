7aecb217f4a64d6ee9239609653b069dc313fcce
Yoel Baez <jbaezgis@gmail.com>
2022-07-19 23:07:08 -0400

    menu principal actualizado - 1.0.14

--- resources/views/livewire/guest-menu.blade.php
+++ resources/views/livewire/guest-menu.blade.php
@@ -1,26 +1,5 @@
-<div class="py-2 bg-gray-50 border-b border-gray-300">
-    <div class="max-w-7xl mx-auto flex justify-between px-2">
-        <div class="flex gap-2">
-            <div>
-                <img class="h-6" src="{{ asset('images/iconos/whatsapp.png') }}" alt="">
-            </div>
-            <div>
-                849-656-6119
-            </div>
-        </div>
-        <div class="flex gap-2">
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-                  </svg>
-            </div>
-            <div>
-                ardemti@gmail.com
-            </div>
-        </div>
-    </div>
-</div>
-<nav x-data="{ open: false }" class="bg-gray-50">
+
+<nav x-data="{ open: false }" class="bg-white border-b border-gray-900">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-center">
@@ -30,9 +9,7 @@
                 </a>
             </div>
         </div>
-        <div class="text-xl text-center font-courgette text-gray-700">
-            Sabor y calidad en cada bocado!
-        </div>
+        
 
         {{-- links --}}
         {{-- <div class="flex gap-2 justify-center">
--- resources/views/livewire/menu-principal.blade.php
+++ resources/views/livewire/menu-principal.blade.php
@@ -1,12 +1,5 @@
 <div>
     <div class="max-w-7xl mx-auto pt-6 px-2">
-        <div class="mb-4 flex justify-center">
-            {{-- <x-jet-button wire:click="createShowModal">
-                {{ __('Ordenar Ahora') }}
-            </x-jet-button> --}}
-           
-        </div>
-
         <div class="flex justify-center pt-4">
             <div class="text-5xl font-anton">
                 Menu
@@ -22,12 +15,12 @@
             <div><img class="h-8" src="{{ asset('images/iconos/taco.png') }}" alt="taco"></div>
         </div>
 
-        <ul class="grid grid-cols-2 gap-x-5 m-10 mx-auto sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+        <ul class="grid grid-cols-2 gap-x-5 m-4 mx-auto sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
             <li class="relative pb-4">
               <input wire:model="categoria" class="sr-only peer" type="radio" value="" name="todas" id="todas">
-              <label class="flex py-2 px-4 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-green-500 peer-checked:ring-2 peer-checked:border-transparent" for="todas">Todo</label>
+              <label class="flex py-2 px-4 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-gray-900 peer-checked:ring-2 peer-checked:border-transparent" for="todas">Todo</label>
           
-              <div class="absolute hidden w-5 h-5 peer-checked:block top-2 right-3 text-green-600">
+              <div class="absolute hidden w-5 h-5 peer-checked:block top-2 right-3">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
@@ -37,9 +30,9 @@
             @foreach ($categoriasMenu as $categoria)
                 <li class="relative pb-4">
                     <input wire:model="categoria" class="sr-only peer" type="radio" value="{{ $categoria->nombre }}" name="{{ $categoria->nombre }}" id="{{ $categoria->nombre }}">
-                    <label class="flex py-2 px-4 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-green-500 peer-checked:ring-2 peer-checked:border-transparent" for="{{ $categoria->nombre }}">{{ $categoria->nombre }}</label>
+                    <label class="flex py-2 px-4 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-gray-900 peer-checked:ring-2 peer-checked:border-transparent" for="{{ $categoria->nombre }}">{{ $categoria->nombre }}</label>
                 
-                    <div class="absolute hidden w-5 h-5 peer-checked:block top-2 right-3 text-green-600">
+                    <div class="absolute hidden w-5 h-5 peer-checked:block top-2 right-3">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
@@ -49,18 +42,18 @@
             @endforeach
         </ul>
 
-        <div wire:poll>
+        <div wire:poll class="max-w-xl mx-auto">
             @foreach ($categorias as $categoria)
-                <div class="mb-4">
-                    <div class="py-6 flex">
-                        <div class="px-2 pt-1 text-gray-900 text-3xl font-bold font-acme border-b-2 border-gray-900">
+                <div class="mb-4 rounded-lg border-4 border-gray-900">
+                    <div class="pb-6">
+                        <div class="px-2 py-2 text-center text-white text-3xl font-bold font-acme bg-gray-900">
                             {{ $categoria->nombre }}
                         </div>
                     </div>
 
                     @foreach ($productos->where('categoria', $categoria->id) as $producto)
-                        <div class="flex gap-2 justify-between">
-                            <div>
+                        <div class="flex gap-2 justify-between px-4">
+                            <div class="mb-2">
                                 <div class="font-acme text-xl font-bold">
                                 {{ $producto->nombre }} 
                                 </div>
@@ -68,9 +61,9 @@
                                     {{ $producto->descripcion }} 
                                 </div>
                             </div>
-                            <div class="grow pt-4">
+                            {{-- <div class="grow pt-4">
                                 <div class="border-b-2 border-dashed border-gray-400"></div>
-                            </div>
+                            </div> --}}
                             <div class="font-acme text-xl font-bold">
                                 RD${{ number_format($producto->precio, 2, '.', ',') }}
                             </div>
